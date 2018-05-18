2b7aa67b12bd645fb14389ef181a87ca32084c41
Lord Sahvith <bulletproofchild@gmail.com>
2018-05-18 14:32:32 -0600

    Imperative vs functionsl reduce, create methods for Phrase Function

--- functional.js
+++ functional.js
@@ -1,3 +1,8 @@
+// Functional Programming with Map, Filter & Reduce
+
+
+// Map Section
+
 let states = ["Kansas", "Nebraska", "North Dakota", "South Dakota"];
 
 // Returns a URL-friendly version of a string
@@ -19,17 +24,21 @@ console.log(imperativeMap(states));
 
 // map: Functional version
 function functionalMap(states) {
-    return states.map(states => urlify(states));
+    return states.map(state => urlify(state));
 }
 
 console.log(functionalMap(states));
 
+// exercise 6.1
 // create full url with string
 function mapURL(states) {
     return states.map(state => "https://example.com/" + urlify(state));
 }
 
-// console.log(mapURL(states));
+console.log(mapURL(states));
+
+
+// Filter section
 
 // filter: Imperative version
 function imperativeFilter(states) {
@@ -51,6 +60,7 @@ function functionalFilter(states) {
 
 console.log(functionalFilter(states));
 
+// exercise 6.2
 // filter: test string includes
 function includesFilter(states) {
     return states.filter(state => state.includes("Dakota"));
@@ -64,3 +74,56 @@ function doubleWordFilter(states) {
 }
 
 console.log(doubleWordFilter(states));
+
+
+// Reduce Section
+
+
+let a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// reduce: Iterative solution
+function iterativeSum(array) {
+    let total = 0;
+    array.forEach(function(n) {
+       total += n; 
+    });
+    return total;
+}
+
+console.log(iterativeSum(a));
+
+// reduce: Functional solution
+function functionalSum(array) {
+    return array.reduce((total, n) => { return total += n});
+}
+
+console.log(functionalSum(a));
+
+// reduce object: Imperative solution
+function imperativeLengths(states) {
+    let lengths = [];
+    states.forEach(function(state) {
+       lengths[state] = state.length; 
+    });
+    return lengths;
+}
+
+console.log(imperativeLengths(states));
+
+// reduce object: Functional solution
+function functionalLengths(states) {
+    return states.reduce((lengths, state) => {
+        lengths[state] = state.length; 
+        return lengths;
+    }, {});
+}
+
+console.log(functionalLengths(states));
+
+// exercise 6.3
+// reduce: get the product of an array
+function productReduce(array) {
+    return array.reduce((total, n) => { return total *= n});
+}
+
+console.log(productReduce(a));
--- palindrome.js
+++ palindrome.js
@@ -3,8 +3,19 @@ function reverse(string) {
     return Array.from(string).reverse().join("");
 }
 
-// Returns true for a plaindrome, false otherwise
-function palindrome(string) {
-    let stringLowercase = string.toLowerCase();
-    return stringLowercase === reverse(stringLowercase);
+// Defines a Phrase Object
+function Phrase(content) {
+    this.content = content;
+
+    // Returns true for a plaindrome, false otherwise
+    this.palindrome = function palindrome() {
+        let processedContent = this.content.toLowerCase();
+        return processedContent === reverse(processedContent);
+    }
+    
+    // exercise 7.1
+    // makes the phrase LOUDER
+    this.louder = function louder() {
+        return this.content.toUpperCase();
+    }
 }
